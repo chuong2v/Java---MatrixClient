37bfab9fb6817db5c64999d91abb029ad75b8ee5
chuon <chuon@DESKTOP-DIQ6B28>
2016-05-04 05:43:05 +0700

    read data from file

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,11 +5,14 @@
  */
 package client;
 
+import filehandler.FileHandler;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import matrix.Matrix;
 import matrix.MatrixException;
 import matrix.SquareMatrix;
@@ -25,32 +28,42 @@ public class Client {
     public static DataInputStream dataInputStream;
 
     public static void main(String[] args) {
+        String fileToServer = "F:\\test\\client.txt";
+        String fileFromServer = "F:\\test\\fromServer.txt";
         try {
-            Socket socket = new Socket("localhost", 2600);
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            objectInputStream = new ObjectInputStream(socket.getInputStream());
-            dataInputStream = new DataInputStream(socket.getInputStream());
-
-            Matrix m1 = SquareMatrix.inputMatrix();
-            System.out.println("The first matrix: ");
-            m1.print();
-            objectOutputStream.writeObject(m1);
-
-            System.out.println("The second matrix: ");
-            Matrix m2 = Matrix.inputMatrix();
-            objectOutputStream.writeObject(m2);
-
-            Matrix m3 = (Matrix) objectInputStream.readObject();
-            System.out.println("The result of m1*m2: ");
-            m3.print();
-
-            double m1Det = dataInputStream.readDouble();
-            System.out.println("The determinant of m1: " + m1Det);
-
-            Matrix m4 = (Matrix) objectInputStream.readObject();
-            m4.print();
-        } catch (IOException | ClassNotFoundException ex) {
-            System.out.println(ex.toString());
+            Socket socket = new Socket("localhost", 2700);
+            FileHandler fileHandler = new FileHandler(socket);
+            fileHandler.send(fileToServer);
+            fileHandler.get(fileFromServer);
+//        try {
+//            Socket socket = new Socket("localhost", 2600);
+//            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+//            objectInputStream = new ObjectInputStream(socket.getInputStream());
+//            dataInputStream = new DataInputStream(socket.getInputStream());
+//
+//            Matrix m1 = SquareMatrix.inputMatrix();
+//            System.out.println("The first matrix: ");
+//            m1.print();
+//            objectOutputStream.writeObject(m1);
+//
+//            System.out.println("The second matrix: ");
+//            Matrix m2 = Matrix.inputMatrix();
+//            objectOutputStream.writeObject(m2);
+//
+//            Matrix m3 = (Matrix) objectInputStream.readObject();
+//            System.out.println("The result of m1*m2: ");
+//            m3.print();
+//
+//            double m1Det = dataInputStream.readDouble();
+//            System.out.println("The determinant of m1: " + m1Det);
+//
+//            Matrix m4 = (Matrix) objectInputStream.readObject();
+//            m4.print();
+//        } catch (IOException | ClassNotFoundException ex) {
+//            System.out.println(ex.toString());
+//        }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
